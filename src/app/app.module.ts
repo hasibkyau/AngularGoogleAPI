b8717b13c3eeeb8c230db07c9096c8945b596b9a
Md. Hasibur Rahman <hasibkyau.cse@gmail.com>
2024-01-10 19:58:21 +0600

    added geocoder component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppComponent } from './app.component';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { GooglemapComponent } from './googlemap/googlemap.component';
 import { RouterModule } from '@angular/router';
+import { GeocoderComponent } from './geocoder/geocoder.component';
 
 @NgModule({
-  declarations: [AppComponent, GooglemapComponent],
+  declarations: [AppComponent, GooglemapComponent, GeocoderComponent],
   imports: [BrowserModule, GoogleMapsModule, RouterModule],
   providers: [],
   bootstrap: [AppComponent],
